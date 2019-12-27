e1204b24256f0d9a2ac79bb5bd80b99e19760eb2
aji <angularworks>
2019-12-27 15:50:45 +0530

    https configurations

--- app.js
+++ app.js
@@ -2,19 +2,25 @@ const child_process = require('child_process'); // To be used later for running 
 const express = require('express');
 const http = require('http');
 const WebSocketServer = require('ws').Server;
+const fs = require('fs')
+const path = require('path')
 
 const app = express();
-const server = http.createServer(app).listen(3000, () => {
+
+var options = {
+    key: fs.readFileSync(path.join('certs', 'key.pem')),
+    cert: fs.readFileSync(path.join('certs', 'crt.pem'))
+}
+const server = http.createServer(options, app).listen(3000, () => {
     console.log('Listening...3000');
 });
 
-// Serve static files out of the www directory, where we will put our HTML page
 app.use(express.static(__dirname + '/public'));
 const io = require('socket.io')(server)
 const ffmpeg = child_process.spawn('ffmpeg', [
     '-i', '-',
-    '-vcodec', 'copy', 
-    '-strict', '-2' , '-f', 'mpegts', 
+    '-vcodec', 'copy',
+    '-strict', '-2', '-f', 'mpegts',
     "udp://127.0.0.1:1234?pkt_size=1316"
 ]);
 
@@ -34,10 +40,10 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log('user-disconnect');
     });
-    socket.on('canvas-data', (msg)=>{
+    socket.on('canvas-data', (msg) => {
         ffmpeg.stdin.write(msg.d);
     })
-    socket.on('canvas-data-stoped', (msg)=>{
+    socket.on('canvas-data-stoped', (msg) => {
         console.log('canvas-data-stoped', msg)
         ffmpeg.kill('SIGINT');
     })
